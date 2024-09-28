SriMani-7/ecomm-microservice

retailer registration

authentication/src/main/java/com/microservices/authentication/dto/RetailerRegister.java
@@ -1,0 +1,11 @@
+package com.microservices.authentication.dto;
+
+import lombok.Data;
+
+@Data
+public class RetailerRegister extends RegistrationRequest {
+	private String shopName;
+	private String GSTIN;
+	private String pannumber;
+	private String Address;
+}

Autowires/microservice-app

src/main/java/com/microservices/app/dto/RetailerRegister.java
@@ -1,0 +1,11 @@
+package com.microservices.app.dto;
+
+import lombok.Data;
+
+@Data
+public class RetailerRegister extends RegisterRequest {
+	private String shopName;
+	private String GSTIN;
+	private String pannumber;
+	private String address;
+}